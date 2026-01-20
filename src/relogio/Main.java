10ed281d3cfcfe127fb6fed15a9ed0843a535af8
Mayra <mayradominik2017@gmail.com>
2026-01-20 16:20:31 -0300

    initial commit: Clock conversion with sealed classes

---
+++ src/relogio/Main.java
@@ -0,0 +1,15 @@
+package relogio;
+
+public class Main {
+    public static void main(String[] args){
+
+        Clock brlclock = new BRLClock();
+        brlclock.setSecond (0);
+        brlclock.setMinute(0);
+        brlclock.setHour(25);
+        
+         System.out.println(brlclock.getTime());
+         System.out.println(new USClock().convert(brlclock).getTime());
+    }
+
+}
